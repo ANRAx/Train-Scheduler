76e3dcb8c3c161dece8db4ca6d1e62a818189ab0
ANRAx <ramdassaj@gmail.com>
2019-12-04 14:48:03 -0500

    start to add pseudocode for JS. Add initial firebase links to head of HTML

---
+++ assets/javascript/traintime.js
@@ -0,0 +1,7 @@
+// Steps Process
+// 1.Create a Firebase link
+// 2. Create initial train data in database 
+// 3. Create button for adding new trains -> update html -> update database
+// 4. Create a way to retrieve trains from the trainlist 
+// 5. Create a way to calculate the time way using difference between start and current time 
+
