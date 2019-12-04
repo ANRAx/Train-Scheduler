7f39b1a5e8af2923cc2a4b342a7bfd231270df83
ANRAx <ramdassaj@gmail.com>
2019-12-04 15:02:05 -0500

    Write onclick event (prevent default submit behavior, grab user inputs, create temp obj. to hold initial train data)

--- assets/javascript/traintime.js
+++ assets/javascript/traintime.js
@@ -5,3 +5,47 @@
 // 4. Create a way to retrieve trains from the trainlist 
 // 5. Create a way to calculate the time way using difference between start and current time 
 
+// +++ Step 1  +++
+// Initialize Firebase
+let firebaseConfig = {
+apiKey: "AIzaSyALK3Eb8Tx1fW9IprvnE6rvQaLCTChLPj0",
+authDomain: "train-time-tracker-2a208.firebaseapp.com",
+databaseURL: "https://train-time-tracker-2a208.firebaseio.com",
+projectId: "train-time-tracker-2a208",
+storageBucket: "train-time-tracker-2a208.appspot.com",
+messagingSenderId: "491611444773",
+appId: "1:491611444773:web:3c2f68dc38344df6eb91cc"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let trainData = firebase.database();
+
+// +++ Step 2 & Step 3 +++
+// Populate Firebase Database with initial data (done in Firebase GUI)
+// Create Buttons for adding trains
+$("add-train-btn").on("click", function(event) {
+    // prevent the default form submit behavior
+    event.preventDefault();
+
+    // grab user inputs
+   let trainName = $("#train-name-input")
+        .val()
+        .trim();
+   let destination = $("#destination-input")
+        .val()
+        .trim();
+   let firstTrain = $("#first-train-input")
+        .val()
+        .trim();
+   let frequency = $("#frequency-input")
+        .val()
+        .trim();
+    // create a local temporary object for holding train data
+   let newTrain = {
+        name: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency
+    };
+})
